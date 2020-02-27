6790e40625a55fd5cee01d8521f0c41c162839a6
Jean Carlos dos Santos <jean0king@gmail.com>
2020-02-26 22:37:16 -0300

    backend  finalizado

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-var numeroEntrada;
+var numeroEntrada = document.getElementById('#numeroEntrada').value;
+var final = 0;
 
 
 function conversor(numeroEntrada) {
+
+    numeroEntrada.toString();
+    var valor = numeroEntrada.split('');
     
+    console.log(valor);
+    var c = valor.length - 1;
+
+    console.log(c);
+
+  for (var index = 0; index < valor.length; index++) {
+      var x = valor[index];
+      y = parseInt(x);
+      if (y == 0) {
+        y = 0;
+      }else{
+        y = 2 ** c;
+      }
+      final = final + y;
+      c--;
+  }
+  resultado();
+ 
 }
+
+function resultado() {
+  alert("Decimal: " + final);
+}
+
+conversor();
+
