126fbbff409b08fe81cb32dd6f60be3055de3efe
jeanAt2 <jean0king@gmail.com>
2020-02-29 13:14:58 -0300

    verificação Finalizada

--- index.js
+++ index.js
@@ -3,27 +3,21 @@ var final = 0;
 
 function conversor(nEntrada) {
 
-  nEntrada = document.getElementById("numeroEntrada").value;
-
-  nEntrada.toString();
-  var valor = nEntrada.split('');
-    
-    console.log(valor);
     var c = valor.length - 1;
 
-    console.log(c);
+    for (var index = 0; index < valor.length; index++) {
+        var x = valor[index];
+        y = parseInt(x);
+        if (y == 0) {
+          y = 0;
+        }else{
+          y = 2 ** c;
+        }
+        final = final + y;
+        c--;
+    }
 
-  for (var index = 0; index < valor.length; index++) {
-      var x = valor[index];
-      y = parseInt(x);
-      if (y == 0) {
-        y = 0;
-      }else{
-        y = 2 ** c;
-      }
-      final = final + y;
-      c--;
-  }
+  
   resultado();
  
 }
@@ -32,4 +26,26 @@ function resultado() {
   alert("Decimal: " + final);
   final =0;
 }
+
+function verificacao() {
+  nEntrada = document.getElementById("numeroEntrada").value
+  nEntrada.toString();
+  valor = nEntrada.split('');
+  var x;
+      valor.forEach(bit => {
+        if (bit == 0 | bit == 1) {
+          x = true;
+        }else{
+          x = false;
+      }}); 
+  
+      if (x ==true) {
+        conversor(nEntrada);
+      }else{
+        final = 0;
+        document.getElementById("numeroEntrada").value = ' ';
+        return alert("Numero inválido.\n Por Favor dige um numero válido ");
+      }
+      
+  }
 
